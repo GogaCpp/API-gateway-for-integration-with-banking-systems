cf6f0e189cc84eaed19d7ca50e0bd61714505d16
GogaCpp <assdddffff54@mail.ru>
2025-03-09 20:04:35 +0300

    add contract

--- src/services/dc_association.py
+++ src/services/dc_association.py
@@ -1,7 +1,6 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 
 from src.database import get_async_session
 from src.models.document_and_contract import DocumentContractAssociation
@@ -15,6 +14,17 @@ class DC_ConnectService():
     ):
         self._session = session
 
+    async def get_connection_by_ids(self, data: ConnectContractDocumentPayload):
+        query = (
+            select(DocumentContractAssociation)
+            .where(
+                DocumentContractAssociation.document_id == data.document_id,
+                DocumentContractAssociation.contract_id == data.contract_id
+            )
+        )
+        result = (await self._session.execute(query)).scalars().first()
+        return result 
+
     async def connect_to_document(self, data: ConnectContractDocumentPayload):
 
         new_association = DocumentContractAssociation(
@@ -27,3 +37,11 @@ class DC_ConnectService():
 
     async def get_connection_list(self):
         return (await self._session.execute((select(DocumentContractAssociation)))).scalars().all()
+
+    async def delete_association(self, data: ConnectContractDocumentPayload):
+
+        con = self.get_connection_by_ids(data)
+        if con is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        await self._session.delete(con)
+        await self._session.commit()
